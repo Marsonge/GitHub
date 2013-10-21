e0ecf00d86acfff7925b3f229981faced95e632f
Tim "Marsonge" Caillot <tim.caillot@gmail.com>
2013-10-21 10:51:51 +0200

    Done the first part of 3
    Corrected.

--- TP2_Exercice_3.c
+++ TP2_Exercice_3.c
@@ -3,17 +3,17 @@
 void main(void)
 {
 //Initialisation des variables
-int uN, vN;
-double uNresult, vNresult;
+int uN, vN, uNresult;
+double vNresult;
 //Affichages
 for (uN=0;uN<=10;uN++)
     {
     uNresult = 3*uN + 5;
-    printf("U(%d) = %lf\n", uN, uNresult);
+    printf("U(%d) = %d\n", uN, uNresult);
     }
 for (vN=0;vN<=10;vN++)
     {
-    vNresult = ((3*(vN*vN)+5))/((vN*vN*vN)+1);
+    vNresult = ((3.0*(vN*vN)+5))/((vN*vN*vN)+1); //Toujours mettre un reel quelque part !
     printf("V(%d) = %lf\n", vN, vNresult);
     }
 }
