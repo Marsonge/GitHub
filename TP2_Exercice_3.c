3b692640c721b57cb76fc6e5a86240071d866012
Tim "Marsonge" Caillot <tim.caillot@gmail.com>
2013-10-21 10:48:05 +0200

    Started Ex3.
    Error somewhere in the format of vNResult, the rest is alright, I guess

--- TP2_Exercice_3.c
+++ TP2_Exercice_3.c
@@ -2,6 +2,18 @@
 
 void main(void)
 {
-
-
+//Initialisation des variables
+int uN, vN;
+double uNresult, vNresult;
+//Affichages
+for (uN=0;uN<=10;uN++)
+    {
+    uNresult = 3*uN + 5;
+    printf("U(%d) = %lf\n", uN, uNresult);
+    }
+for (vN=0;vN<=10;vN++)
+    {
+    vNresult = ((3*(vN*vN)+5))/((vN*vN*vN)+1);
+    printf("V(%d) = %lf\n", vN, vNresult);
+    }
 }
